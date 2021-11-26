bc4b22919557767289b421d61f8fcb6498417dc5
Rabia <sdetcode@gmail.com>
2021-11-27 00:13:39 +0530

    product service

---
+++ src/main/java/com/product/service/entity/Product.java
@@ -0,0 +1,14 @@
+package com.product.service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor(staticName = "of")
+public class Product {
+
+    private int productId;
+    private String description;
+    private double price;
+
+}
